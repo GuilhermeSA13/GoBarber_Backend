df07d4ebb31f75646f186c0d8ac358622c5a9c0b
Guilherme Souza Alves <souzaalves333@gmail.com>
2021-03-05 15:16:53 -0300

    Primeiro commit plataforma de barbeiro

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { request } from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ Message: "Hello World" });
+})
+
+app.listen(3333, () => {
+  console.log("Server Start!!!");
+})
