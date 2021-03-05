48cd1d5354f1ea4356b2f920a6f8701fd0e6d1ad
Guilherme Souza Alves <souzaalves333@gmail.com>
2021-03-05 16:10:42 -0300

    Cadastro de agendamentos

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4'
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.get('/', (requets, response) => {
+  return response.json(appointments);
+})
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+  const appointment = {
+    id: uuid(),
+    provider,
+    date
+  }
+
+  appointments.push(appointment);
+
+  return response.json(appointment)
+})
+
+export default appointmentsRouter;
