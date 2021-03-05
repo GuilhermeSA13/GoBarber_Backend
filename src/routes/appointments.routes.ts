5e917d30d133b9f3426137eea6e59d76b8926be2
Guilherme Souza Alves <souzaalves333@gmail.com>
2021-03-05 17:24:16 -0300

    Criaçao do repositório de appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.get('/', (requets, response) => {
   return response.json(appointments);
@@ -15,8 +15,8 @@ appointmentsRouter.post('/', (request, response) => {
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentsInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentsInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentsInSameDate) {
@@ -24,9 +24,8 @@ appointmentsRouter.post('/', (request, response) => {
       .status(400)
       .json({ message: 'This appointment is already booked' });
   }
-  const appointment = new Appointment(provider, parsedDate);
 
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import routes from './routes'
-
+import routes from './routes';
 
 const app = express();
 
@@ -8,5 +7,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Server Start!!!");
-})
+  console.log('Server Start!!!');
+});
